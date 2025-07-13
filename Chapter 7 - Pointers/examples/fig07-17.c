80dab08811462ab3077065591fc66e347f0f5496
Ideadly-Skies <Obie.kal22@gmail.com>
2025-07-13 15:30:38 +1000

    feat(chapter-7): added function pointers

---
+++ Chapter 7 - Pointers/examples/fig07-17.c
@@ -0,0 +1,16 @@
+// fig07_17.c
+// Multipurpose sorting program using function pointers
+#include <stdio.h>
+#define SIZE 10
+
+// prototypes
+void bubbleSort(int work[], size_t size, int (*compare)(int a, int b));
+int ascending(int a, int b);
+int descending(int a, int b);
+
+int main(void) {
+    
+
+
+    return (0);
+}
