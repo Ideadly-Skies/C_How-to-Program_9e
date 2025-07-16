725410ec1fbf89ec702e6ce4cba9d7ce365868c0
Ideadly-Skies <Obie.kal22@gmail.com>
2025-07-16 19:31:20 +1000

    feat(chapter-7): created a random maze from scratch using DFS!

--- Chapter 7 - Pointers/exercises/7.20.c
+++ Chapter 7 - Pointers/exercises/7.20.c
@@ -1,36 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define ROWS 12
 #define COLS 13
 
 // starting X and Y values of maze
-int startX = 2, startY = 0;
+int startX = 1, startY = 1;
 
 // ending X and Y values of
-int endX = 4;
-int endY = 11;     
+int endX = 10;
+int endY = 11;
+
+// constants to generate maze
+#define WALL_CHAR '#'
+#define EMPTY_CHAR '.'
+#define DIRECTION_UP 0
+#define DIRECTION_LEFT 1
+#define DIRECTION_RIGHT 2
+#define DIRECTION_DOWN 3
+
+// generate map
+void depth_first_search(int current_row, int current_col, int rows, int cols, char maze[ROWS][COLS+1]);
+void get_available_direction(int current_row, int current_col, int rows, int cols, char maze[ROWS][COLS+1], int available_directions[4]);
 
 void printMaze(char maze[ROWS][COLS+1]); // print current maze 
 int recursiveSolve(int x, int y, char maze[ROWS][COLS+1], char wasHere[ROWS][COLS+1], char correctPath[ROWS][COLS+1]); // prototype
 
+// https://humbertowoody.xyz/posts/maze-generator-game-in-c#main-function
 int main(void){
+    srand(time(NULL));
+
     // allocate one more space for null 
     // terminating character
-    char maze[ROWS][COLS+1] = {
-        "############",
-        "#...#......#",
-        "..#.#.####.#",
-        "###.#....#.#",
-        "#....###.#..",
-        "####.#.#.#.#",
-        "#..#.#.#.#.#",
-        "##.#.#.#.#.#",
-        "#........#.#",
-        "######.###.#",
-        "#......#...#",
-        "############"
-    };
+    // char maze[ROWS][COLS+1] = {
+    //     "############",
+    //     "#...#......#",
+    //     "..#.#.####.#",
+    //     "###.#....#.#",
+    //     "#....###.#..",
+    //     "####.#.#.#.#",
+    //     "#..#.#.#.#.#",
+    //     "##.#.#.#.#.#",
+    //     "#........#.#",
+    //     "######.###.#",
+    //     "#......#...#",
+    //     "############"
+    // };
+
+    // init maze
+    char maze[ROWS][COLS+1];
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            maze[i][j] = WALL_CHAR;
+        }
+        maze[i][COLS] = '\0'; // null-terminate each row
+    }
+
+    depth_first_search(1, 1, ROWS, COLS, maze);
+
+    // set starting pos of maze
+    maze[1][1] = EMPTY_CHAR;
+
+    // set ending pos of maze
+    maze[10][11] = EMPTY_CHAR;
 
     // maze variables
     char wasHere[ROWS][COLS+1]; 
@@ -59,6 +93,96 @@ int main(void){
     }
 
     return (0);
+}
+
+void depth_first_search(int current_row, int current_col, int rows, int cols, char maze[ROWS][COLS+1]){
+    int available_directions[4] = {0, 0, 0, 0}, // array for storing all of the available directions for each cell
+        direction;                              // the selected direction 
+
+    do {
+        maze[current_row][current_col] = EMPTY_CHAR;
+
+        // get the updated and available directions for the current cell
+        get_available_direction(current_row, current_col, rows, cols, maze, available_directions);        
+
+        // check for the base case for our recursion: no available directions.
+        if (!available_directions[DIRECTION_UP] && !available_directions[DIRECTION_DOWN] && !available_directions[DIRECTION_LEFT] && !available_directions[DIRECTION_RIGHT]){
+            return;
+        } 
+
+        do {
+            // Random value between 0 and 3
+            direction = rand() % 4;
+        } while (available_directions[direction] == 0);
+
+        switch (direction) {
+            case DIRECTION_UP:
+                // Carve wall
+                maze[current_row - 1][current_col] = EMPTY_CHAR;
+
+                // Move to next cell
+                depth_first_search(current_row - 2, current_col, rows, cols, maze);
+                break;
+
+            case DIRECTION_LEFT:
+                maze[current_row][current_col - 1] = EMPTY_CHAR;
+                depth_first_search(current_row, current_col - 2, rows, cols, maze);
+                break;
+
+            case DIRECTION_RIGHT:
+                maze[current_row][current_col + 1] = EMPTY_CHAR;
+                depth_first_search(current_row, current_col + 2, rows, cols, maze);
+                break;
+
+            case DIRECTION_DOWN:
+                maze[current_row + 1][current_col] = EMPTY_CHAR;
+                depth_first_search(current_row + 2, current_col, rows, cols, maze);
+                break;
+        }
+
+    } while (available_directions[DIRECTION_UP] || available_directions[DIRECTION_DOWN] || available_directions[DIRECTION_LEFT] || available_directions[DIRECTION_RIGHT]);
+}
+
+void get_available_direction(int current_row, int current_col, int rows, int cols, char maze[ROWS][COLS+1], int available_directions[4]){
+    // DIRECTION_UP
+    if ((current_row - 2) >= 1 && maze[current_row - 2][current_col] == WALL_CHAR)
+    {
+        available_directions[DIRECTION_UP] = 1;
+    }
+    else
+    {
+        available_directions[DIRECTION_UP] = 0;
+    }
+
+    // DIRECTION_DOWN
+    if ((current_row + 2) <= (rows - 2) && maze[current_row + 2][current_col] == WALL_CHAR)
+    {
+        available_directions[DIRECTION_DOWN] = 1;
+    }
+    else
+    {
+        available_directions[DIRECTION_DOWN] = 0;
+    }
+
+    // DIRECTION_RIGHT
+    if ((current_col + 2) <= (cols - 2) && maze[current_row][current_col + 2] == WALL_CHAR)
+    {
+        available_directions[DIRECTION_RIGHT] = 1;
+    }
+    else
+    {
+        available_directions[DIRECTION_RIGHT] = 0;
+    }
+
+    // DIRECTION_LEFT
+    if ((current_col - 2) >= 1 && maze[current_row][current_col - 2] == WALL_CHAR)
+    {
+        available_directions[DIRECTION_LEFT] = 1;
+    }
+    else
+    {
+        available_directions[DIRECTION_LEFT] = 0;
+    }
 }
 
 int recursiveSolve(int x, int y, char maze[ROWS][COLS+1], char wasHere[ROWS][COLS+1], char correctPath[ROWS][COLS+1]){
